2f7b3cc7ecc2344ca1b9368fcdac8ded1c658254
Pri1902 <Priyankapraveeenajain1902@gmail.com>
2026-03-24 09:45:54 +0530

    Display Consist Summary

--- TrainConsistManagementApp/src/TrainConsistManagementApp.java
+++ TrainConsistManagementApp/src/TrainConsistManagementApp.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
 public class TrainConsistManagementApp {
     public static void main (String[] args) {
 
+        System.out.println("====================================");
+        System.out.println("=== Train Consist Management App ===");
+        System.out.println("====================================");
+
+        List<String> trainConsist = new ArrayList<>();
+
+
+        System.out.println("Initial bogie count: " + trainConsist.size());
+        System.out.println("Current train consist: " + trainConsist);
+
+
+        System.out.println("System Ready For Operation....");
     }
 }
