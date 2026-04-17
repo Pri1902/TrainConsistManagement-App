535603be70707f27c0b6535d98ff8bdeb6f33c4d
Pri1902 <Priyankapraveenajain1902@gmail.com>
2026-04-17 10:38:01 +0530

    UC11: Validate Train ID & Cargo Codes

--- TrainConsistManagementApp/src/TrainConsistManagementApp.java
+++ TrainConsistManagementApp/src/TrainConsistManagementApp.java
@@ -1,32 +1,38 @@
+import java.util.regex.*;
 import java.util.*;
-import java.util.stream.*;
 
 public class TrainConsistManagementApp {
-    public static class Bogie {
-            String type;
-            int capacity;
-
-            Bogie(String type, int capacity) {
-                this.type = type;
-                this.capacity = capacity;
-
-        }
-    }
-
-
     public static void main(String[] args) {
 
-        List<Bogie> bogies = Arrays.asList(
-                new Bogie("First Class", 24),
-                new Bogie("Cargo", 120),
-                new Bogie("Sleeper", 72),
-                new Bogie("AC Chair", 56)
-        );
+        Scanner sc = new Scanner(System.in);
 
-        int totalCapacity = bogies.stream()
-                .map(b -> b.capacity)        // extract capacity
-                .reduce(0, Integer::sum);   // sum all values
+        System.out.print("Enter Train ID (format: TRN-1234): ");
+        String trainId = sc.nextLine();
 
-        System.out.println("Total Seating Capacity: " + totalCapacity);
+        System.out.print("Enter Cargo Code (format: PET-AB): ");
+        String cargoCode = sc.nextLine();
+
+        String trainRegex = "TRN-\\d{4}";
+        String cargoRegex = "PET-[A-Z]{2}";
+
+        Pattern trainPattern = Pattern.compile(trainRegex);
+        Pattern cargoPattern = Pattern.compile(cargoRegex);
+
+        Matcher trainMatcher = trainPattern.matcher(trainId);
+        Matcher cargoMatcher = cargoPattern.matcher(cargoCode);
+
+        if (trainMatcher.matches()) {
+            System.out.println("Train ID is VALID");
+        } else {
+            System.out.println("Train ID is INVALID");
+        }
+
+        if (cargoMatcher.matches()) {
+            System.out.println("Cargo Code is VALID");
+        } else {
+            System.out.println("Cargo Code is INVALID");
+        }
+
+        sc.close();
     }
 }
