f350ed46c4cb81fc691990bc0be6e3ced04bd7c8
Pri1902 <priyankapraveenajain1902@gmail.com>
2026-04-27 01:34:05 +0530

    UC17: Sort Bogie Names Using Arrays.sort()

--- TrainConsistManagementApp/src/TrainConsistManagementApp.java
+++ TrainConsistManagementApp/src/TrainConsistManagementApp.java
@@ -1,66 +1,31 @@
 import java.util.*;
 
 public class TrainConsistManagementApp {
-    // Method to perform Bubble Sort
-    public static void bubbleSort(int[] capacities) {
-        int n = capacities.length;
-
-        // Outer loop → number of passes
-        for (int i = 0; i < n - 1; i++) {
-
-            // Inner loop → compare adjacent elements
-            for (int j = 0; j < n - i - 1; j++) {
-
-                // If left > right → swap
-                if (capacities[j] > capacities[j + 1]) {
-
-                    // Swapping logic
-                    int temp = capacities[j];
-                    capacities[j] = capacities[j + 1];
-                    capacities[j + 1] = temp;
-                }
-            }
-        }
-    }
-
-    // Method to display array
-    public static void display(int[] arr) {
-        for (int val : arr) {
-            System.out.print(val + " ");
-        }
-        System.out.println();
-    }
 
     public static void main(String[] args) {
-    System.out.println("========================================");
-        System.out.println("UC16 - Sort Passenger Bogie Capacities");
+     System.out.println("========================================");
+        System.out.println("UC17 - Sort Bogie Type Names");
         System.out.println("========================================\n");
 
-        Scanner sc = new Scanner(System.in);
-
-        // User input
-        System.out.print("Enter number of bogies: ");
-        int n = sc.nextInt();
-
-        int[] capacities = new int[n];
-
-        System.out.println("Enter capacities:");
-        for (int i = 0; i < n; i++) {
-            capacities[i] = sc.nextInt();
-        }
+        // Array of bogie types
+        String[] bogieTypes = {
+            "First Class",
+            "Sleeper",
+            "AC Chair",
+            "General",
+            "Luxury"
+        };
 
         // Before sorting
-        System.out.print("\nBefore Sorting: ");
-        display(capacities);
+        System.out.println("Before Sorting:");
+        System.out.println(Arrays.toString(bogieTypes));
 
-        // Bubble Sort
-        bubbleSort(capacities);
+        // Using built-in sort
+        Arrays.sort(bogieTypes);
 
         // After sorting
-        System.out.print("After Sorting: ");
-        display(capacities);
+        System.out.println("\nAfter Sorting (Alphabetical):");
+        System.out.println(Arrays.toString(bogieTypes));
 
-        System.out.println("\nSorting completed using Bubble Sort.");
-        System.out.println("Program continues safely...");
     }
 }
