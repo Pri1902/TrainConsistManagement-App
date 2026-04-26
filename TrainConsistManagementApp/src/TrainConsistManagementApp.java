9aed5b13b6d265d13c5c70fbfb111817a7bb03c0
Pri1902 <priyankapraveenajain1902@gmail.com>
2026-04-27 01:39:27 +0530

    UC18: Search Bogie ID Through Linear Search

--- TrainConsistManagementApp/src/TrainConsistManagementApp.java
+++ TrainConsistManagementApp/src/TrainConsistManagementApp.java
@@ -3,29 +3,49 @@ import java.util.*;
 public class TrainConsistManagementApp {
 
     public static void main(String[] args) {
-     System.out.println("========================================");
-        System.out.println("UC17 - Sort Bogie Type Names");
+    System.out.println("========================================");
+        System.out.println("UC18 - Search Bogie ID (Linear Search)");
         System.out.println("========================================\n");
 
-        // Array of bogie types
-        String[] bogieTypes = {
-            "First Class",
-            "Sleeper",
-            "AC Chair",
-            "General",
-            "Luxury"
-        };
+        Scanner sc = new Scanner(System.in);
 
-        // Before sorting
-        System.out.println("Before Sorting:");
-        System.out.println(Arrays.toString(bogieTypes));
+        // Input: number of bogies
+        System.out.print("Enter number of bogies: ");
+        int n = sc.nextInt();
+        sc.nextLine(); 
 
-        // Using built-in sort
-        Arrays.sort(bogieTypes);
+        // Array of bogie IDs
+        String[] bogieIDs = new String[n];
 
-        // After sorting
-        System.out.println("\nAfter Sorting (Alphabetical):");
-        System.out.println(Arrays.toString(bogieTypes));
+        // Input bogie IDs
+        System.out.println("Enter bogie IDs:");
+        for (int i = 0; i < n; i++) {
+            bogieIDs[i] = sc.nextLine();
+        }
 
+        // Search key
+        System.out.print("\nEnter bogie ID to search: ");
+        String key = sc.nextLine();
+
+        boolean found = false;
+
+        // Linear Search
+        for (int i = 0; i < n; i++) {
+
+            // Compare using equals()
+            if (bogieIDs[i].equals(key)) {
+                System.out.println("Bogie ID found at position: " + i);
+                found = true;
+                break; // Early termination
+            }
+        }
+
+        // Result if not found
+        if (!found) {
+            System.out.println("Bogie ID not found.");
+        }
+
+        System.out.println("\nSearch completed.");
+        System.out.println("Program continues safely...");
     }
 }
